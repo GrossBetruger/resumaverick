1b7d57d578e5101748186754eac7e23cd15e5e63
GrossBetruger <koriatoren@gmail.com>
2025-05-18 21:55:25 +0300

    feat: spin gcp gpu cmd

---
+++ spin_spot_gpu_gcp.sh
@@ -0,0 +1,12 @@
+ gcloud compute instances create my-spot-gpu \
+  --zone=europe-west1-b \
+  --machine-type=n1-standard-4 \
+  --accelerator=type=nvidia-tesla-t4,count=1 \
+  --maintenance-policy=TERMINATE \
+  --provisioning-model=SPOT \
+  --image-family=pytorch-latest-gpu \
+  --image-project=deeplearning-platform-release \
+  --boot-disk-size=100GB \
+  --boot-disk-type=pd-ssd \
+  --metadata='install-nvidia-driver=True'
+
